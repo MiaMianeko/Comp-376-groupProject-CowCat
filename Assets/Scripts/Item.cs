77bbd19f81eb860e722849de9e543853c9dd66a2
Antoine <antoine.dufourblain@gmail.com>
2021-10-30 17:23:12 -0400

    minor updates to classes

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // Text to be shown when item is "looked at" in the inventory
+    [SerializeField] DialogData examineText;
+
+    void Start()
+    {
+        
+    }
+
+    //Method to display text to be shown
+    void examineItem()
+    {
+
+    }
+}
