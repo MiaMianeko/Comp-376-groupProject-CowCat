238827a27c88db7f366eb54681b880520837ea09
Honggeer <honggeer1208@gmail.com>
2021-11-03 22:40:15 -0400

    Added note image

---
+++ Assets/Scripts/Chapter1/Classroom/NoteOnChair.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteOnChair : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool canInteract;
+
+    public NoteOnChair()
+    {
+        canInteract = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
