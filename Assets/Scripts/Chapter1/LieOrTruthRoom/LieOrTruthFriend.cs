69711f6f4c42481862b0a097679964a058337ed6
zhangyuxuan <yuxuan001@89trillion.com>
2021-11-13 21:27:19 -0500

    add Friends movement

--- Assets/Scripts/Chapter1/LieOrTruthRoom/LieOrTruthFriend.cs
+++ Assets/Scripts/Chapter1/LieOrTruthRoom/LieOrTruthFriend.cs
@@ -6,18 +6,99 @@ using UnityEngine.PlayerLoop;
 
 public class LieOrTruthFriend : Interactable
 {
+    private Rigidbody2D _rigidbody2D;
     [SerializeField] private GameObject dialogGameObject;
     private Dialog _dialog;
     private UserInput _userInput;
     public int roundNumber = 1;
+    public bool isMove = false;
+    public bool isFacingRight =false;
+    public bool isFacingLeft = false;
+    public bool isFacingUp = false;
+    public bool isFacingDown =false;
+ 
+    private float speed = 10.0f;
+     
+    public Vector3 direction = new Vector3( 0, 0, 0);
+    
 
     void Start()
     {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
         _userInput = FindObjectOfType<UserInput>();
+        direction = new Vector3(-1, 0, 0);
     }
 
     void Update()
     {
+        
+        
+        Vector3 delta = direction * speed * Time.fixedDeltaTime;
+        _rigidbody2D.MovePosition(_rigidbody2D.position + new Vector2(delta.x, delta.y));
+        
+       
+            if (direction.x < 0)
+            {
+                isMove = true;
+                isFacingLeft = true;
+                isFacingUp = false;
+                isFacingDown = false;
+                isFacingRight = false;
+                
+            }
+            else if (direction.x > 0)
+            {
+                isMove = true;
+                isFacingLeft = false;
+                isFacingUp = false;
+                isFacingDown = false;
+                isFacingRight = true;
+            }
+            else
+            {
+                isMove = false;
+                if (direction.y > 0)
+                {
+                    isMove = true;
+                    isFacingLeft = false;
+                    isFacingUp = true;
+                    isFacingDown = false;
+                    isFacingRight = false;
+                }
+
+                else if (direction.y < 0)
+                {
+                    isMove = true;
+                    isFacingLeft = false;
+                    isFacingUp = false;
+                    isFacingDown = true;
+                    isFacingRight = false;
+                }
+                else
+                {
+                    isMove = false;
+                }
+            }
+           
+
+           
+
+           
+
+            print("isFacingLeft"+ isFacingLeft + isMove);
+        
+       
+        GetComponent<Animator>().SetBool("isFacingLeft", isFacingLeft);
+        GetComponent<Animator>().SetBool("isFacingDown", isFacingDown);
+        GetComponent<Animator>().SetBool("isFacingUp", isFacingUp);
+        GetComponent<Animator>().SetBool("isFacingRight", isFacingRight);
+        GetComponent<Animator>().SetBool("isMove", isMove);
+      
+
+
+       
+        
+        
         if (canInteract && Input.GetKey(KeyCode.F))
         {
             canInteract = false;
