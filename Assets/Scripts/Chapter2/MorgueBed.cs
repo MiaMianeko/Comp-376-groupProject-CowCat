51275f165bc958861eb276fa9e3a1aa831c8d3d8
Antoine <antoine.dufourblain@gmail.com>
2021-11-17 16:56:20 -0500

    Additional tweaks.
    Trying to figure out how to use items

---
+++ Assets/Scripts/Chapter2/MorgueBed.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorgueBed : Interactable
+{
+    [SerializeField] private GameObject dialogGameObject;
+    private Dialog _dialog;
+    private UserInput _userInput;
+
+
+    private void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        if (canInteract && Input.GetKey(KeyCode.F))
+        {
+            canInteract = false;
+
+            _userInput = FindObjectOfType<UserInput>();
+            _userInput.canMove = false;
+
+            dialogGameObject.SetActive(true);
+            _dialog = FindObjectOfType<Dialog>();
+
+
+            
+            StartCoroutine(FileReader.GetText(
+               Application.streamingAssetsPath + "/Dialogs/HospitalMorgueCorpseCheck.json",
+               jsonData =>
+               {
+                   DialogData dialogData = JsonUtility.FromJson<DialogData>(jsonData);
+                   StartCoroutine(_dialog.OutputDialog(dialogData, () =>
+                      {
+                         dialogGameObject.SetActive(false);
+                         _userInput.canMove = true;
+                      }));
+               }));
+             
+        }
+    }
+}
