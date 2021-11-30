707e901860a0f07ed4595819399560481a2b50f0
SuperTyler <896267775@qq.com>
2021-11-29 20:31:30 -0500

    Fix main menu BGM

--- Assets/Scripts/Menu/MainMenuBGM.cs
+++ Assets/Scripts/Menu/MainMenuBGM.cs
@@ -8,6 +8,7 @@ public class MainMenuBGM : MonoBehaviour
 
     void Start()
     {
+        print(FindObjectsOfType<MainMenuBGM>().Length);
         if (FindObjectsOfType<MainMenuBGM>().Length == 1)
         {
             bgmSource.Play();
@@ -18,5 +19,7 @@ public class MainMenuBGM : MonoBehaviour
     public void StopBGM()
     {
         bgmSource.Stop();
+        Destroy(bgmSource);
+        Destroy(this);
     }
 }
