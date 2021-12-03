2d6e23a9299dadd116755d71840ae5cc19673f82
zhangyuxuan <yuxuan001@89trillion.com>
2021-12-02 20:13:40 -0500

    Update Bad Ending

--- Assets/Scripts/Epilogue/BE/ChasingScene/ChasingSceneGameManager.cs
+++ Assets/Scripts/Epilogue/BE/ChasingScene/ChasingSceneGameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChasingSceneGameManager : MonoBehaviour
 {
@@ -10,9 +11,12 @@ public class ChasingSceneGameManager : MonoBehaviour
     private UserController _player;
 
     [SerializeField] private GameObject dialogGameObject;
+
     private Dialog _dialog;
-    [SerializeField] private GameObject gameOverGameObject;
-    
+    // [SerializeField] private GameObject gameOverGameObject;
+
+    [SerializeField] private AudioSource bgmAudio;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,22 +24,21 @@ public class ChasingSceneGameManager : MonoBehaviour
         _player = FindObjectOfType<UserController>();
         Invoke(nameof(LoadDialog1), 1.0f);
         _player.canMove = false;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-    
+
     public void LoadDialog1()
     {
         dialogGameObject.SetActive(true);
         _dialog = FindObjectOfType<Dialog>();
-    
 
-        StartCoroutine(FileReader.GetText(Application.streamingAssetsPath + "/Dialogs/EpilogueBEBEGameManagerDialog1.json",
+
+        StartCoroutine(FileReader.GetText(
+            Application.streamingAssetsPath + "/Dialogs/EpilogueBEBEGameManagerDialog1.json",
             jsonData =>
             {
                 DialogData dialogData = JsonUtility.FromJson<DialogData>(jsonData);
@@ -50,7 +53,27 @@ public class ChasingSceneGameManager : MonoBehaviour
             }));
     }
 
+    public void StopBGM()
+    {
+        bgmAudio.Stop();
+    }
+    
+    public void LoadDialog2()
+    {
+        dialogGameObject.SetActive(true);
+        _dialog = FindObjectOfType<Dialog>();
 
+
+        StartCoroutine(FileReader.GetText(
+            Application.streamingAssetsPath + "/Dialogs/EpilogueBEBEGameManagerDialog2.json",
+            jsonData =>
+            {
+                DialogData dialogData = JsonUtility.FromJson<DialogData>(jsonData);
+                StartCoroutine(_dialog.OutputDialog(dialogData, () =>
+                {
+                    dialogGameObject.SetActive(false);
+                    SceneManager.LoadScene("ClassroomEnding");
+                }));
+            }));
+    }
 }
-
-
